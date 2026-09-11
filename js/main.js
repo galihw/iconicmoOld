029996d72c9b06f611625c5e0cf8c6fb0b9e0b1b
galih w <72421097+galihw@users.noreply.github.com>
2026-09-11 19:58:49 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
 document.querySelectorAll('.btnDetail').forEach(item => {
     item.addEventListener('click', (e) => {
+		
+		//sembunyikan katalog
+		document.getElementById("katalog").style.display = "none";
+		document.getElementById("productDetail").style.display = "";
+		
+		document.getElementById("btnkembali").addEventListener('click', (e) => {
+			document.getElementById("katalog").style.display = "";
+			document.getElementById("productDetail").style.display = "none";
+		})
+		
         let parent = e.target.parentNode.parentNode;
 
         let gambar = parent.querySelector('.card-img-top').src;
-		console.log(gambar)
         let color = parent.querySelector('.card-img-top').style.backgroundColor;
+		
 		
 		// mengambil nomor di string gambar
 		let hasil1 = gambar.charAt(gambar.length-7);
@@ -13,23 +23,18 @@ document.querySelectorAll('.btnDetail').forEach(item => {
 		let no = 1*(hasil1+''+hasil2+''+hasil3);
 		if(hasil1=='s') no = 1*(hasil2+''+hasil3);
 		else if(hasil2=='s') no = 1*(hasil3);
-		//console.log(hasil1,hasil2,no);
-		//console.log(parent.id)
 		gambar = 'images/'+(noklik+1)+'/kaos'+no+'.png';
 		let gambar2 = 'images/'+(noklik+1)+'/orang'+no+'.jpg';
-		//console.log(hasil1,hasil2,hasil3);
-		
-		
-		
 		
         let harga = parent.querySelector('.harga').innerHTML;
         let judul = parent.querySelector('.card-text').innerHTML;
         let deskripsi = parent.querySelector('.deskripsi') ? parent.querySelector('.deskripsi').innerHTML : '<i>tidak ada informasi yang tersedia</i>';
 
 		
-        let tombolModal = document.querySelector('.btnModal');
-        tombolModal.click();
+        //let tombolModal = document.querySelector('.btnModal');
+        //tombolModal.click();
 
+		
         document.querySelector('.modalTitle').innerHTML = judul;
         let image = document.createElement('img');
 		image.style.backgroundColor = color;
@@ -44,13 +49,13 @@ document.querySelectorAll('.btnDetail').forEach(item => {
         document.querySelector('.modalDeskripsi').innerHTML = deskripsi;
         document.querySelector('.modalHarga').innerHTML = harga;
 
-        const nohp = '6285714408830';
-        let pesan = `https://api.whatsapp.com/send?phone=${nohp}&text=Halo Bang, saya mau pesan produk ini ${judul}`;
-        document.querySelector('.btnBeli').href = pesan;
+        //const nohp = '6285714408830';
+        //let pesan = `https://api.whatsapp.com/send?phone=${nohp}&text=Halo Bang, saya mau pesan produk ini ${judul}`;
+        //document.querySelector('.btnBeli').href = pesan;
 		
-        //document.querySelector('.btnBeli').addEventListener('click', () => {
-		//  pay(parseInt(harga),judul);
-		//});
+        document.querySelector('.btnBeli').addEventListener('click', () => {
+		  pay(parseInt(harga),judul);
+		});
 		
     });
 });
